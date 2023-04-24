12e9f0c192b71b2fff0ee0c34bbf599281848b3d
anyababkina <aa.babkina16@gmail.com>
2023-04-24 19:12:37 +0300

    add tests, file structure, cache

--- books/urls.py
+++ books/urls.py
@@ -7,11 +7,11 @@ from .views import *
 
 urlpatterns = [
     path('', cache_page(60 * 5)(home), name='home'),
-    path('general_store/', cache_page(60 * 5)(StoreListView.as_view()), name='store'),
+    path('general_store/', StoreListView.as_view(), name='store'),
     path('contact_us/', cache_page(60 * 5)(contact_us), name='contacts'),
     path('books/<int:book_id>/', cache_page(60 * 5)(AboutBookView.as_view()), name='about_book'),
     path('author/<int:author_id>/', cache_page(60 * 5)(AboutAuthorView.as_view()), name='about_author'),
-    path('genres/<int:genre_id>/', cache_page(60 * 5)(GenresListView.as_view()), name='genres'),
-    path('subgenres/<int:subgenre_id>/', cache_page(60 * 5)(SubGenresListView.as_view()), name='subgenres'),
+    path('genres/<int:genre_id>/', GenresListView.as_view(), name='genres'),
+    path('subgenres/<int:subgenre_id>/', SubGenresListView.as_view(), name='subgenres'),
     path('searching/', SearchBookListView.as_view(), name='searching'),
 ]
