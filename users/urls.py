759e2e86b7887dc83a1e541b3e60ce0b9fe73994
anyababkina <aa.babkina16@gmail.com>
2023-04-24 19:13:57 +0300

    cache

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('profile/', lk, name='lk'),
     path('order/', order, name='order'),
-    path('orders/<int:order_id>/', cache_page(60)(AboutOrderView.as_view()), name='about_order'),
+    path('orders/<int:order_id>/', cache_page(60 * 2)(AboutOrderView.as_view()), name='about_order'),
     path('basket/<int:book_id>', add_basket, name='add_basket'),
     path('d_basket/<int:book_id>', delete_basket, name='delete_basket'),
 ]
